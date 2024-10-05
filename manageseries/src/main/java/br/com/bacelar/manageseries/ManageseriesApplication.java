38d63a1a20e4aa5bfb01f5f99eb199b87d9db3a7
Viniciusovski <vini98.2012@gmail.com>
2024-10-05 13:36:43 -0300

    Projeto Spring sem Web

--- manageseries/src/main/java/br/com/bacelar/manageseries/ManageseriesApplication.java
+++ manageseries/src/main/java/br/com/bacelar/manageseries/ManageseriesApplication.java
@@ -1,13 +1,18 @@
 package br.com.bacelar.manageseries;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ManageseriesApplication {
+public class ManageseriesApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ManageseriesApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Primeiro projeto Spring sem Web");
+	}
 }
