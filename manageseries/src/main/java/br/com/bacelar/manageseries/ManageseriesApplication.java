d38731124602f5f4145739291218134b38acda9d
Viniciusovski <vini98.2012@gmail.com>
2024-10-05 13:28:25 -0300

    Criação do Projeto

---
+++ manageseries/src/main/java/br/com/bacelar/manageseries/ManageseriesApplication.java
@@ -0,0 +1,13 @@
+package br.com.bacelar.manageseries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageseriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageseriesApplication.class, args);
+	}
+
+}
---
+++ manageseries/src/test/java/br/com/bacelar/manageseries/ManageseriesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bacelar.manageseries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageseriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
