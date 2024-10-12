b86cdae87f527a8f728b980a44b2c26f3ff4e385
Viniciusovski <vini98.2012@gmail.com>
2024-10-11 22:21:48 -0300

    Menu de interação com o usuário

---
+++ manageseries/src/main/java/br/com/bacelar/manageseries/principal/Principal.java
@@ -0,0 +1,40 @@
+package br.com.bacelar.manageseries.principal;
+
+import br.com.bacelar.manageseries.model.DadosEpisodio;
+import br.com.bacelar.manageseries.model.DadosSerie;
+import br.com.bacelar.manageseries.model.DadosTemporada;
+import br.com.bacelar.manageseries.service.ConsumoAPI;
+import br.com.bacelar.manageseries.service.ConverteDados;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Principal {
+    private Scanner leitura = new Scanner(System.in);
+    private ConsumoAPI consumoApi = new ConsumoAPI();
+    private ConverteDados conversor = new ConverteDados();
+    private final String ENDERECO = "https://omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=6585022c";
+
+    public void exibeMenu(){
+        System.out.println("Digite o nome da série para busca");
+        var nomeSerie = leitura.nextLine();
+        var json = consumoApi.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+        System.out.println(dados);
+
+		List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for(int i = 1; i<=dados.totalTemporadas(); i++){
+			json = consumoApi.obterDados(ENDERECO + nomeSerie.replace(" ", "+")+ "&season=" + i + API_KEY);
+			DadosTemporada dadosTemporada = conversor.obterDados(json,DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+        // For each implementando system out
+	    // temporadas.forEach(System.out::println);
+
+        // Percorre para recuperar apenas o nome dos episodios
+        temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+    }
+}
