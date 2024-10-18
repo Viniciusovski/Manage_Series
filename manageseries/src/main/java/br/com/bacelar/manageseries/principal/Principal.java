8c8767cb97f1f6e42c97c638bd9f0087530e3c6d
Viniciusovski <vini98.2012@gmail.com>
2024-10-17 21:58:25 -0300

    Manipulando com fluxos as coleções de dados

--- manageseries/src/main/java/br/com/bacelar/manageseries/principal/Principal.java
+++ manageseries/src/main/java/br/com/bacelar/manageseries/principal/Principal.java
@@ -3,13 +3,14 @@ package br.com.bacelar.manageseries.principal;
 import br.com.bacelar.manageseries.model.DadosEpisodio;
 import br.com.bacelar.manageseries.model.DadosSerie;
 import br.com.bacelar.manageseries.model.DadosTemporada;
+import br.com.bacelar.manageseries.model.Episodio;
 import br.com.bacelar.manageseries.service.ConsumoAPI;
 import br.com.bacelar.manageseries.service.ConverteDados;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Principal {
     private Scanner leitura = new Scanner(System.in);
@@ -38,13 +39,51 @@ public class Principal {
         // Percorre para recuperar apenas o nome dos episodios
         temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
 
-        List<String> nomes = Arrays.asList("Vinícius", "Leonardo", "Galadriel", "Lúcio", "Rihanna");
+//        List<String> nomes = Arrays.asList("Vinícius", "Leonardo", "Galadriel", "Lúcio", "Rihanna");
+//
+//        nomes.stream()
+//                .sorted()
+//                .limit(3)
+//                .filter(n -> n.startsWith("L"))
+//                .map(n -> n.toUpperCase())
+//                .forEach(System.out::println);
 
-        nomes.stream()
-                .sorted()
-                .limit(3)
-                .filter(n -> n.startsWith("L"))
-                .map(n -> n.toUpperCase())
+        // FlatMap é usado para "achatar" uma estrutura de dados complexa em uma estrutura mais simples
+        // Pega cada um dos episódios e aglutiná-os na mesma lista
+        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+
+        //Ordenar por avaliação de forma decrescente os top 5 melhores epsisodios
+        System.out.println("\nTop 5 episódios");
+        dadosEpisodios.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                .limit(5)
                 .forEach(System.out::println);
+
+        List<Episodio> episodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream()
+                        .map(d -> new Episodio(t.numero(), d))
+                ).collect(Collectors.toList());
+
+        episodios.forEach(System.out::println);
+
+        System.out.println("\nA partir de que ano você deseja ver os episódios? ");
+        var ano = leitura.nextInt();
+        leitura.nextLine();
+
+        LocalDate dataBusca = LocalDate.of(ano, 1, 1);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        episodios.stream()
+                .filter(e -> e != null && e.getDataLancamento().isAfter(dataBusca))
+                .forEach(e -> System.out.println(
+                        "Temporada: " + e.getTemporada() +
+                                "\nEpisódio: " + e.getTitulo() +
+                                "\nData de Lançamento: " + e.getDataLancamento().format(formatter) +
+                                "\n*************************************************"
+                ));
+
     }
 }
